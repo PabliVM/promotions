d09dbda3fa5c9c01f3c701a3457db5e67abfe5b0
PabliVM <pablo.vegamartin@gmail.com>
2026-07-02 12:35:42 +0200

    Update campograma-core-new.js

--- js/campograma-core-new.js
+++ js/campograma-core-new.js
@@ -24,7 +24,6 @@ let dia   = sessionStorage.getItem("rm_dia") || "LUNES"; // día activo global
 let FECHAS = calcFechasSemana();
 const origen = {};
 let movimientos = {}; // movimientos[dia][eq][nombre] = {ts, user}
-const _dispColapsado = new Set(); // equipos con zona disponibles colapsada
 let porteros = []; // array de nombres marcados como portero
 // ══════════════════════════════════════════════════
 // ESTADO
